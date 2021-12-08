7f357951c4f4fc4d67de72b2fe36c82f1789f967
jacobkostek <jacobkostek@gmail.com>
2021-12-08 23:31:12 +0100

    Dodaj pliki projektów.

---
+++ Lets go/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Lets_go
+{
+    internal class Program
+    {
+        static int CzyLitera(char a)
+        {
+            string literki = "Bab";
+            //literki = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            int y = 0;
+            while (y<literki.Length)
+            {
+                if (a == literki[y])
+                {
+                    return 1;
+                }
+                else
+                {
+                    y++;
+                }
+            }
+            return 0;
+
+
+
+        }
+
+
+        static int CzySamogloska(char a)
+        {
+            string literki = "a";
+            //literki = "AaEeYyUuIiOo";
+            int y = 0;
+            while (y < literki.Length)
+            {
+                if(a == literki[y])
+                {
+                    return 1;
+                }
+                else
+                {
+                    y++;
+                }
+            }
+            return 0;
+        }
+
+
+        static string Znajdz(string tekst)
+        {
+            int y = 0;
+            string wyraz = "";
+            string najlepszywyraz = "";
+            int[] tab = new int[10];
+            while (y < tekst.Length)
+            {
+                if (CzyLitera(tekst[y]) == 1)
+                {
+                    wyraz = wyraz + tekst[y];
+                    y++;
+                }
+                else
+                {
+                    if (wyraz != "")
+                    {
+                        int samogloska = 0;
+                        int spolgloska = 0;
+                        int a = 0;
+                        int roznica = 0;
+
+                        while (a<wyraz.Length)
+                        {
+                            if(CzySamogloska(wyraz[a]) == 1)
+                            {
+                                samogloska++;
+                                a++;
+                            }
+                            else
+                            {
+                                spolgloska++;
+                                a++;
+                            }
+                        }
+                        if(roznica < spolgloska - samogloska)
+                        {
+                            roznica = spolgloska - samogloska;
+                            najlepszywyraz = wyraz;
+                            wyraz = "";
+                            samogloska = 0;
+                            spolgloska = 0;
+                            a = 0;
+                        }
+                        else
+                        {
+                            wyraz = "";
+                            samogloska = 0;
+                            spolgloska = 0;
+                            a = 0;
+                        }
+
+                        
+                    }
+                    else
+                    {
+                        y++;
+                    }
+                }
+            }
+
+
+            if (najlepszywyraz == "") return " ";
+            else return najlepszywyraz;
+        }
+        static void Main(string[] args)
+        {
+            string tekst = "ba ab ba bbb";
+            Console.WriteLine(Znajdz(tekst));
+        }
+    }
+}
---
+++ PROBNY TEST/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace PROBNY_TEST
+{
+    internal class Program
+    {
+        static bool CzySamogloska(char a)
+        {
+            string literki = "iyeaouIYEAOU";
+            //literki = "a";
+            int y = 0;
+            while (y < literki.Length)
+            {
+                if (a == literki[y])
+                {
+                    return true;
+
+                }
+                else
+                {
+                    y++;
+                }
+            }
+            return false;
+
+        }
+
+        static bool CzyLitera(char a)
+        {
+            string literki = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            //literki = "ab";
+            int y = 0;
+            while (y < literki.Length)
+            {
+                if (a == literki[y])
+                {
+                    return true;
+                }
+                else
+                {
+                    y++;
+                }
+            }
+            return false;
+        }
+        static string Znajdz(string tekst)
+        {
+            int samogloski = 0;
+            int spolgloski = 0;
+            int y = 0;
+            int a = 0;
+            string wyraz = "";
+            string najlepszywyraz = "";
+            while (y < tekst.Length)
+            {
+                while (y < tekst.Length)
+                {
+                    if (true == CzyLitera(tekst[y]))
+                    {
+                        wyraz = wyraz + tekst[y];
+                        y++;
+
+                    }
+                    else
+                    {
+                        y++;
+                        if (wyraz != "") break;
+
+
+                    }
+                }
+
+
+                while (wyraz.Length > a)
+                {
+                    if (true == CzySamogloska(wyraz[a]))
+                    {
+                        samogloski++;
+                        a++;
+                    }
+                    else if (false == CzySamogloska(wyraz[a]))
+                    {
+                        spolgloski++;
+                        a++;
+                    }
+                }
+                if (spolgloski > samogloski)
+                {
+                    najlepszywyraz = wyraz;
+                    wyraz = "";
+                    spolgloski = 0;
+                    samogloski = 0;
+                }
+                else
+                {
+                    spolgloski = 0;
+                    samogloski = 0;
+                    wyraz = "";
+                }
+
+
+            }
+
+                    
+                
+            
+
+
+
+
+
+            if (najlepszywyraz == "")
+            {
+                return " ";
+
+            }
+            else return najlepszywyraz;
+        }
+        static void Main(string[] args)
+        {
+            string tekst = " aa-bbb?ppp*yyt ";
+            Console.WriteLine(Znajdz(tekst));
+        }
+    }
+}
